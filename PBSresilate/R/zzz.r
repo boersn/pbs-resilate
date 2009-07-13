fbb80c1cd090c71148a9cff9473764c19acb3e0c
rowan.haigh <rowan.haigh@dfo-mpo.gc.ca>
2009-07-13 23:05:30 +0000

    PBSresilate v.0.14 built and posted to FTP and SRF. Fixes issue 2.

--- PBSresilate/R/zzz.r
+++ PBSresilate/R/zzz.r
@@ -1,11 +1,9 @@
 .First.lib <- function(lib, pkg) {
 cat(
-"\nPBS Resilate 0.13 -- Copyright (C) 2008 Fisheries & Oceans Canada
+"\nPBS Resilate 0.14 -- Copyright (C) 2008-09 Fisheries & Oceans Canada
 
-Built on Feb 18, 2009
+Built on Jul 13, 2009
 Pacific Biological Station, Nanaimo
 
-Type 'runResilate()' to start the model control GUI.
-
-\"We may not be big, but we're small.\"\n\n")
+Type 'runResilate()' to start the model control GUI.\n\n")
 }
