3b6bd3e5734ddbc085b523624792d54fbf9fdd71
rowan.haigh@dfo-mpo.gc.ca <rowan.haigh@dfo-mpo.gc.ca@12e5e7f6-6f2f-11de-9c52-fb6994a310fd>
2012-11-27 17:49:50 +0000

    Updated to be compatible with R-2.15.0 and higher. Added call to 'utils::globalVariables' in zzz.r to indicate 'no visible bindings' when checking. Changed Jon's email address.

--- trunk/PBSresilate/R/zzz.r
+++ trunk/PBSresilate/R/zzz.r
@@ -4,24 +4,39 @@
 	if( is.character( pkg_info$Packaged ) )
 		pkg_date <- strsplit( pkg_info$Packaged, " " )[[1]][1]
 	else
-		pkg_date  <- "unkown"
+		pkg_date  <- date()
 	
 	userguide_path <- system.file( "doc/PBSresilate.pdf", package = "PBSresilate" )
 	
-	cat("
+	packageStartupMessage("
 -----------------------------------------------------------
-PBSresilate", pkg_info$Version, "-- Copyright (C) 2008-2011 Fisheries and Oceans Canada
+PBSresilate ", pkg_info$Version, " -- Copyright (C) 2008-2012 Fisheries and Oceans Canada
 
 A basic user guide 'PBSresilate.pdf' is located at 
 ", userguide_path, "
 
-Packaged on", pkg_date, "
+Packaged on ", pkg_date, "
 Pacific Biological Station, Nanaimo
+
+All available PBS packages can be found at
+http://code.google.com/p/pbs-software/
 
 Type 'runResilate()' to start the model control GUI.
 -----------------------------------------------------------
 
-
 ")
 }
+
+# No Visible Bindings
+# ===================
+if(getRversion() >= "2.15.1") utils::globalVariables(names=c(
+	"addXY","addXZ","addYZ",
+	"histclr",
+	"myGrad",
+	"p23","PBSresi",
+	"size2d","size3d","solver","states",
+	"t0","t1","type3d",
+	"xyz",
+	"y1","y2","y3"
+	), package="PBSresilate")
 
